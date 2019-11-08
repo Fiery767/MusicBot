f847e3376af8eb96caafe71b60b5326e03ec0d1b
Melody Wang <Wang99814>
2019-11-08 10:36:58 -0800

    changed layout a little

--- Rude.java
+++ Rude.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */ 
-public class Rude extends SongReferences
+public class Rude extends MusicBot
 {
     // instance variables - replace the example below with your own
     private int x;
